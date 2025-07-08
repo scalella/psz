08dbd192e006badf0deeaf1ab27254a15f5c5638
Martin Scalella <101156252+scalella@users.noreply.github.com>
2025-07-08 16:34:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,53 @@
-$(document).ready(function() {
-    $('.portfolio').each(function(index) {
-        $(this).attr('id', 'img' + (index + 1));
-    });
+//Toggling Menu
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
 
-    $('.portfolio').each(function() {
-        $('#navi').append('<div class="circle"></div>');
-    });
+    if(toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');
+        })
+    }
+}
 
-    $('.circle').each(function(index) {
-        $(this).attr('id', 'circle' + (index + 1));
-    });
+showMenu('nav-toggle', 'nav-menu');
 
-    $('.portfolio').click(function() {
-        if ($(this).hasClass('opened')) {
-            $(this).removeClass('opened');
-            $(".portfolio").fadeIn("fast");
-            $(this).find('.ombra').fadeOut();
-            $("#navi div").removeClass("activenav");
-        } else {
-            var indexi = $("#maincontent .portfolio").index(this) + 1;
-            $(this).addClass('opened');
-            $(".portfolio").not(this).fadeOut("fast");
-            $(this).find('.ombra').fadeIn();
-            $("#circle" + indexi).addClass('activenav');
-        }
-    });
+//Toggling Active Link
+const navLink = document.querySelectorAll('.nav-link');
 
-    //navi buttons
-    $("#navi div").click(function() {
-        if ($(this).hasClass("activenav")) {
-            return false;
-        }
+function linkAction() {
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
 
-        $("#navi div").removeClass("activenav");
-        $(".portfolio").removeClass('opened');
-        $(".portfolio").show();
-        $('.ombra').hide();
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
+}
 
-        var index = $("#navi div").index(this) + 1;
-        $("#img" + index).addClass('opened');
-        $(".portfolio").not("#img" + index).fadeOut("fast");
-        $("#img" + index).find('.ombra').fadeIn();
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
-        $(this).addClass("activenav");
-    });
-});
+// Scroll Reveal
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.home-title', {} )
+sr.reveal('.button', {delay: 200} )
+sr.reveal('.home-img', {delay: 400} )
+sr.reveal('.home-social', {delay: 400,} )
+
+sr.reveal('.about-img', {} )
+sr.reveal('.about-subtitle', {delay: 200} )
+sr.reveal('.about-text', {delay: 400} )
+
+sr.reveal('.skills-subtitle', {delay: 100} )
+sr.reveal('.skills-text', {delay: 150} )
+sr.reveal('.skills-data', {interval: 200} )
+sr.reveal('.skills-img', {delay: 400} )
+
+sr.reveal('.work-img', {interval: 200} )
+
+sr.reveal('.contact-input', {interval: 200} )
